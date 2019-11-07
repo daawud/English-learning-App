553404257f37c275cea9c216681301b839ace323
avgorobec <avgorobec@gmail.com>
2019-11-07 19:54:13 +0300

    Модуль Header

--- client/app/Header/containers/Header.jsx
+++ client/app/Header/containers/Header.jsx
@@ -1,28 +1,53 @@
 import './Header.scss';
-import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import { MDBNavbar, MDBNavbarBrand,  MDBNavbarNav,    MDBNavbarToggler,    MDBCollapse,    MDBNavItem,    MDBIcon,    MDBBtn} from 'mdbreact';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+
+import NavbarRight from '~/components/HeaderNavbarRight/HeaderNavbarRight.jsx';
+import registerFormReducer from '~/components/Register/reducer.js';
 
 class Header extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            collapse: false,
+            isWideEnough: false
+        };
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick() {
+        this.setState({
+            collapse: !this.state.collapse
+        });
     }
 
     render() {
         return (
-            <>
-                <div className="row mt-2 ">
-                    <Link to='/auth' className="auth-btn"> ВОЙТИ </Link>
-                </div>
-            </>
+            <div>
+                <Router>
+                    <header>
+                        <MDBNavbar className="bg-secondary" dark expand="md" scrolling fixed="top">
+                            <MDBNavbarBrand>
+                                <Link to="/" className="btn bg-light" data-toggle="tooltip" title="Перейти на главную страницу">Logotip</Link>
+                            </MDBNavbarBrand>
+                            <MDBNavbarToggler onClick={ this.onClick } />
+                            <MDBCollapse isOpen = { this.state.collapse } navbar>
+                                <NavbarRight userLogged={this.props.userAuthenticateStatus}/>
+                            </MDBCollapse>
+                        </MDBNavbar>
+                    </header>
+                </Router>
+            </div>
         );
     }
 }
 
 function mapStateToProps(state) {
-    return state;
+    return {
+        userAuthenticateStatus: state.registerFormReducer.isLoggedIn,
+    }
 }
 
 export default connect(mapStateToProps)(Header);

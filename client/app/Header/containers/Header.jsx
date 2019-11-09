f5317d1677c7eb7c42de95c35f2da0b42e743e8b
avgorobec <avgorobec@gmail.com>
2019-11-09 13:33:43 +0300

    73 Добавил исправления послу code review

--- client/app/Header/containers/Header.jsx
+++ client/app/Header/containers/Header.jsx
@@ -1,4 +1,5 @@
 import './Header.scss';
+import {MDBNavbarBrand} from 'mdbreact';
 import React, {Component} from 'react';
 import {  Link } from 'react-router-dom';
 
@@ -13,21 +14,27 @@ class Header extends Component {
     }
 
     render() {
+        let loginUser;
+        if (this.state.userIsLogged) {
+            loginUser = (
+                <>
+                    <span className="text-white" data-toggle="tooltip" title="Количество доступных очков для открытия новых уроков">100</span>
+                    <HeaderUserPic />
+                    <Link to="/cabinet" className="btn bg-light" data-toggle="tooltip" title="Войти в личный кабинет">Войти в личный кабинет</Link>
+                </>)
+            } else {
+                loginUser = (<Link to="/auth" className="btn bg-light" data-toggle="tooltip" title="Войти в учетную запись">Войти</Link>);
+            }
+
         return (
             <header className="bg-secondary d-flex justify-content-between align-items-center" >
-                <div>
+                <MDBNavbarBrand>
                     <Link to="/" className="btn bg-light" data-toggle="tooltip" title="Перейти на главную страницу">Logotip</Link>
-                </div>
+                    <a className="btn btn-primary" data-toggle="popover" data-img="https://mdbootstrap.com/img/logo/mdb192x192.jpg">Hover
+                        me</a>
+                </MDBNavbarBrand>
                 <div>
-                    { (this.state.userIsLogged) ?
-                        <>
-                            <span className="text-white" data-toggle="tooltip" title="Количество доступных очков для открытия новых уроков">100</span>
-                            <HeaderUserPic />
-                            <Link to="/cabinet" className="btn bg-light" data-toggle="tooltip" title="Войти в личный кабинет">Войти в личный кабинет</Link>
-                        </>
-                         :
-                        <Link to="/auth" className="btn bg-light" data-toggle="tooltip" title="Войти в учетную запись">Войти</Link>
-                    }
+                    {loginUser}
                 </div>
             </header>
         );

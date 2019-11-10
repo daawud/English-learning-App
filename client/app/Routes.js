4ac1003c476490c5f7016cd5be64e5af434a85c3
Andrey <35691656+fufylev@users.noreply.github.com>
2019-11-10 20:46:49 +0500

    1. Сделал модуль входа пользователя 2. Создал класс валидации полей 3. Поправил файлы корневой папки чтобы это все заработало

--- client/app/Routes.js
+++ client/app/Routes.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import MainPage from '~/layouts/MainPage/MainPage.jsx';
-import PageNotFound from '~/PageNotFound/PageNotFound.jsx';
 
 function Routes() {
     return (
         <Router>
             <Switch>
                 <Route path="/" component={MainPage}/>
-                <Route path="*" component={PageNotFound}/>
             </Switch>
         </Router>
     );
--- client/app/index.js
+++ client/app/index.js
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { createLogger } from 'redux-logger';
 
 import Routes from './Routes';
 import rootReducers from './rootReducers';
 import rootSaga from './rootSaga';
 
 const saga = createSagaMiddleware();
-const store = createStore(combineReducers(rootReducers), applyMiddleware(saga));
+const store = createStore(combineReducers(rootReducers), applyMiddleware(saga, createLogger()));
 
 saga.run(rootSaga);
 

55a20126ebf3908d9cc55272c1e70cfd2fa82f91
Svetik5 <33523034+Svetik5@users.noreply.github.com>
2019-11-11 17:20:18 +0300

    Изменения Андрея Корехова

--- basic/controllers/UsersController.php
+++ basic/controllers/UsersController.php
@@ -2,7 +2,9 @@
 
 namespace app\controllers;
 
+use app\models\User;
 use app\models\Users;
+use yii\db\Exception;
 use yii\rest\ActiveController;
 
 class UsersController extends ActiveController
@@ -11,6 +13,7 @@ class UsersController extends ActiveController
 
     public function behaviors()
     {
+
         return [
             [
                 'class' => \yii\filters\ContentNegotiator::className(),
@@ -21,4 +24,52 @@ class UsersController extends ActiveController
         ];
     }
 
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['update'], $actions['create'], $actions['delete']);
+
+        return $actions;
+    }
+
+    public function actionUpdate(){
+        $k = 0;
+
+        try {
+            $user = Users::find()->where(['userId'=>\Yii::$app->request->get('id')])->one();
+
+            if (\Yii::$app->request->get('name') !== NULL) {
+                $user->firstName = \Yii::$app->request->get('name');
+                $k++;
+            }
+            if (\Yii::$app->request->get('lastname') !== NULL) {
+                $user->lastName = \Yii::$app->request->get('lastname');
+                $k++;
+            }
+            if (\Yii::$app->request->get('birthDate') !== NULL) {
+                $user->birthDate = \Yii::$app->request->get('birthDate');
+                $k++;
+            }
+            if (\Yii::$app->request->get('points') !== NULL) {
+                $user->points = \Yii::$app->request->get('points');
+                $k++;
+            }
+            if ($k == 0) {
+                return "Не указаны данные для изменения";
+            }
+
+            $user->save();
+            return "Ok";
+        } catch (Exception $x) {
+            return "Что то пошло не так $x";
+        }
+    }
+
+    public function actionCreate() {
+        return "Воспользуйтесь модулем аутенфикации";
+    }
+
+    public function actionDelete() {
+        return "Воспользуйтесь модулем аутенфикации";
+    }
 }

8dd97e86a775f76aea8725307069660fba40ba47
victoriaCat <katysheva.victoria@gmail.com>
2019-11-13 19:22:34 +0300

    71. Добавлены первоначальные стили

--- client/app/ForgotPassword/components/ForgotPasswordEmail.jsx
+++ client/app/ForgotPassword/components/ForgotPasswordEmail.jsx
@@ -22,7 +22,7 @@ class ForgotPasswordEmail extends Component {
                     ЭЛЕКТРОННАЯ ПОЧТА
                 </label>
                 <input
-                    className="form-control mb-4"
+                    className="form-control mb-3"
                     type="email"
                     maxLength="50"
                     id="forgotPasswordFormEmail"
--- client/app/ForgotPassword/containers/ForgotPassword.jsx
+++ client/app/ForgotPassword/containers/ForgotPassword.jsx
@@ -6,7 +6,7 @@ import { Redirect, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { forgotPasswordModalToOpen, forgotPasswordModalToClose, emailOnBlur } from '~/ForgotPassword/actions';
-import  ForgotPasswordEmail  from '~/ForgotPassword/components/ForgotPasswordEmail.jsx';
+import ForgotPasswordEmail from '~/ForgotPassword/components/ForgotPasswordEmail.jsx';
 
 class ForgotPassword extends Component {
     constructor(props) {
@@ -25,25 +25,27 @@ class ForgotPassword extends Component {
         return (
             <>
                 <MDBModal isOpen={this.props.forgotPasswordModalOpened}
-                          toggle={() => this.props.dispatch(forgotPasswordModalToClose())}>
+                          toggle={() => this.props.dispatch(forgotPasswordModalToClose())}
+                          className="forgot-password-modal">
                     {!this.props.forgotPasswordModalOpened && <Redirect to="/"/>}
-                    <MDBModalBody>
-                        <p className="text-center">Забыли пароль?
-                            <MDBCloseIcon
-                                onClick={() => this.props.dispatch(forgotPasswordModalToClose())}>
-                            </MDBCloseIcon>
-                        </p>
-                        <form onSubmit={this.handleSubmit} className="text-center p-5">
+                    <MDBModalBody className="forgot-password-modal__body">
+                        <form onSubmit={this.handleSubmit}
+                              className="text-center px-5 position-relative forgot-password-form">
+                            <h5 className="mt-2 mb-5">Забыли пароль?</h5>
+                            <MDBCloseIcon className="forgot-password-form__close"
+                                          onClick={() => this.props.dispatch(forgotPasswordModalToClose())}/>
                             <p className="h6 text-center mb-4">
                                 Мы отправим на Вашу электронную почту новый пароль
                             </p>
                             <ForgotPasswordEmail value={this.props.value}/>
-                            <MDBBtn className="btn btn-info btn-block my-4" type="submit">
-                                Отправить
+                            <MDBBtn
+                                className="col-md-6 offset-md-3 btn btn-dark btn-block my-4 border-white rounded-pill"
+                                type="submit">
+                                ОТПРАВИТЬ
                             </MDBBtn>
                         </form>
                         <p className="text-center"> Нет личного кабинета?
-                            <Link to="/register"> Зарегистрируйся </Link>
+                            <Link className="forgot-password-form__link" to="/register"> Зарегистрируйся </Link>
                         </p>
                     </MDBModalBody>
                 </MDBModal>

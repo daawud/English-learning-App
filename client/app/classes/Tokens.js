64fd62acb7a68af30f94d85c78d6a114c7d15c53
avgorobec <avgorobec@gmail.com>
2019-11-20 14:56:29 +0300

    105. Добавил метод извлечения токенов в класс Tokens

--- client/app/classes/Tokens.js
+++ client/app/classes/Tokens.js
@@ -13,4 +13,16 @@ export default class Tokens {
         localStorage.setItem('token', token['token']);
         localStorage.setItem('refreshToken', token['refreshToken']);
     }
+    /**
+     * Метод извлечения access и refresh из LocalStorage
+     * @return {Object} - объект с access и refresh токеном
+     */
+    getFromLocalStorage() {
+        const tokens = {};
+
+        tokens.token = localStorage.setItem('token', token['token']);
+        tokens.refreshToken = localStorage.setItem('refreshToken', token['refreshToken']);
+
+        return tokens;
+    }
 }

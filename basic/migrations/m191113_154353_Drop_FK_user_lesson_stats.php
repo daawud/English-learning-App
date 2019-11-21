c2e5c061cdba69f15fa278b35776f039c0f4fe35
MarinaMich <mar7995@yandex.ru>
2019-11-21 20:23:36 +0300

    133 Изменения в таблице users: переименование id в userId, добавление граф imgUrl, reg_date, изменение свойств first_name, birth_date

---
+++ basic/migrations/m191113_154353_Drop_FK_user_lesson_stats.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m191113_154353_Drop_FK_user_lesson_stats
+ */
+class m191113_154353_Drop_FK_user_lesson_stats extends Migration
+{
+    /**
+     * удаляем FK_user_lesson_stats_users1, что бы была возможность изменить поле id в таблице users 
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey(
+            'fk_user_lesson_stats_users1',
+            'user_lesson_stats'
+        );
+
+        $this->dropIndex(
+            'idx_user_lesson_stats_users1',
+            'user_lesson_stats'
+        );
+    }
+      
+}
---
+++ basic/migrations/m191113_155147_Rename_column_users.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+use yii\db\Schema;
+
+/**
+ * Class m191113_155147_Rename_column_users
+ */
+class m191113_155147_Rename_column_users extends Migration
+{
+    /**
+     * изменяем наименования столбца в таблице users
+     */
+    public function safeUp()
+    {
+        $sql = "ALTER TABLE `users` 
+            CHANGE COLUMN `id` `userId` VARCHAR(36) NOT NULL;";
+       
+       $this->execute($sql);
+    }
+
+    
+    public function safeDown()
+    {
+        $sql = "ALTER TABLE `users` 
+            CHANGE COLUMN `userId` `id` VARCHAR(36) NOT NULL;";
+
+        $this->execute($sql);
+    }
+}

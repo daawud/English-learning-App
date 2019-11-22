919515d35f0504155f4bf2bc82f37316d09856be
Andrey <35691656+fufylev@users.noreply.github.com>
2019-11-22 15:44:00 +0300

    137 Рефакторинг модуля Footer
    137 немного подправил футер для того чтобы он нормально собирался на мобилке. После появления макета мобильной версии отредактирую повторно.

--- client/app/Footer/Footer.jsx
+++ client/app/Footer/Footer.jsx
@@ -1,43 +1,51 @@
 import './Footer.scss';
 
 import React from 'react';
+import {Link} from 'react-router-dom';
+import { MDBCol, MDBContainer, MDBRow, MDBFooter, MDBBtn } from 'mdbreact';
 
-import crest from '~/assets/img/crest.svg';
+import crest from '~/assets/img/crest_icon.svg';
 import facebook from '~/assets/img/facebook.svg';
 import youtube from '~/assets/img/youtube.svg';
 import vkontakte from '~/assets/img/vkontakte.svg';
 import instagram from '~/assets/img/insta.svg';
 
+
 const Footer = (props) => {
     return (
-        <div className="footer footer-ela">
-            <div className="layer layer-2">
-                <p className="footer-logo">eLA</p>
-            </div>
-            <div className="layer layer-1">
-                <div className="layer__text">
-                    <p>О проекте</p>
-                    <p>Условия и <br/>политика</p>
-                </div>
-            </div>
-            <div className="layer">
-                <div className="layer__text">
-                    <p>Справка</p>
-                    <p>ПОддержка</p>
-                    <p>Приложения</p>
-                </div>
-
-            </div>
-            <div className="footer-icons">
-                <img className="position-absolute footer-icons__crest" src={crest} alt=""/>
-                <img className="position-absolute footer-icons__facebook" src={facebook} alt=""/>
-                <img className="position-absolute footer-icons__youtube" src={youtube} alt=""/>
-                <img className="position-absolute footer-icons__vkontakte" src={vkontakte} alt=""/>
-                <img className="position-absolute footer-icons__instagram" src={instagram} alt=""/>
-            </div>
-
-
-        </div>
+        <MDBFooter className="m-0 p-0">
+            <MDBContainer fluid className="">
+                <MDBRow className="footer-ela layer">
+                    <MDBCol md="3" sm="12" className="layer layer-1 d-flex justify-content-center align-items-center">
+                        <Link to="/" data-toggle="tooltip" title="Перейти на главную страницу">
+                            <span className="footer-logo">eLA</span>
+                        </Link>
+                    </MDBCol>
+                    <MDBCol md="6" sm="12" className="layer layer-2 d-flex justify-content-end align-items-center">
+                        <div className="layer__text p-0 mr-3">
+                            <Link to="/"><p>О проекте</p></Link>
+                            <Link to="/"><p>Условия и <br/>политика</p></Link>
+                        </div>
+                    </MDBCol>
+                    <MDBCol md="9" sm="12" className="layer layer-3 d-flex justify-content-end align-items-center">
+                        <div className="layer__text p-0 mr-3">
+                            <Link to="/"><p>Справка</p></Link>
+                            <Link to="/"><p>Поддержка</p></Link>
+                            <Link to="/"><p>Приложения</p></Link>
+                        </div>
+                    </MDBCol>
+                    <MDBCol md="3" sm="12" className="m-0 p-0 layer layer-4 d-flex justify-content-center align-items-center">
+                        <div className="footer-icons m-0 p-0">
+                            <img className="footer-icons__crest" src={crest} alt=""/>
+                            <a href="https://www.facebook.com/"><img className="footer-icons__facebook" src={facebook} alt=""/></a>
+                            <a href="https://www.youtube.com/"><img className="footer-icons__youtube" src={youtube} alt=""/></a>
+                            <a href="https://vk.com/"><img className="footer-icons__vkontakte" src={vkontakte} alt=""/></a>
+                            <a href="https://www.instagram.com/"><img className="footer-icons__instagram" src={instagram} alt=""/></a>
+                        </div>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        </MDBFooter>
     );
 };
 

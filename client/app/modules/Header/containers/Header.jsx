13e33f6643193947b15b3f04a1a5b6a897ca1418
victoriaCat <katysheva.victoria@gmail.com>
2019-11-25 21:22:31 +0300

    150. Сверстана кнопка Выйти из личного кабинета
    Без функционала + произведена небольшая правка стилей

--- client/app/modules/Header/containers/Header.jsx
+++ client/app/modules/Header/containers/Header.jsx
@@ -6,6 +6,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import HeaderUserPic from '~/modules/Header/components/HeaderUserPic/HeaderUserPic.jsx';
+import SignOutButton from '~/modules/Header/components/SignOutButton/SignOut.jsx';
 import AuthForm from '~/modules/Auth/containers/Auth.jsx';
 import RegisterForm from '~/modules/Register/containers/Register.jsx';
 import ForgotPasswordForm from '~/modules/ForgotPassword/containers/ForgotPassword.jsx';
@@ -18,7 +19,7 @@ class Header extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isOpen: false
+            isOpen: false,
         };
         this.toggleCollapse = this.toggleCollapse.bind(this);
     }
@@ -42,13 +43,14 @@ class Header extends Component {
                             <img src={man} alt="курс"/>
                         </div>
                     </MDBNavLink>
-                </div>)
+                    <SignOutButton/>
+                </div>);
         } else {
             userBlock = (
                 <MDBBtn className="sign-in-btn"
-                    data-toggle="tooltip"
-                    title="Войти в учетную запись"
-                    onClick={() => this.props.dispatch(authFormToOpen())}>
+                        data-toggle="tooltip"
+                        title="Войти в учетную запись"
+                        onClick={() => this.props.dispatch(authFormToOpen())}>
                     Войти
                 </MDBBtn>
             );
@@ -56,14 +58,14 @@ class Header extends Component {
 
         return (
             <>
-                {this.props.authModalOpened && <AuthForm />}
-                {this.props.registerModalOpened && <RegisterForm />}
-                {this.props.forgotPasswordModalOpened && <ForgotPasswordForm />}
+                {this.props.authModalOpened && <AuthForm/>}
+                {this.props.registerModalOpened && <RegisterForm/>}
+                {this.props.forgotPasswordModalOpened && <ForgotPasswordForm/>}
                 <MDBNavbar className="header_bg" color="indigo" dark expand="md">
                     <MDBNavLink className="logo-block p-0" to="/"><span className="logo-text">eLA</span></MDBNavLink>
-                    <MDBNavbarToggler onClick={this.toggleCollapse} />
+                    <MDBNavbarToggler onClick={this.toggleCollapse}/>
                     <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
-                        <div className="buttons-set container-fluid d-flex justify-content-end mr-5">
+                        <div className="buttons-set container-fluid d-flex justify-content-center mr-auto">
                             {userBlock}
                         </div>
                     </MDBCollapse>

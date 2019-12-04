1e320d3fdee3fca91835c5404dfd1827bded2487
victoriaCat <katysheva.victoria@gmail.com>
2019-12-04 11:30:49 +0300

    158. Поправки согласно замечаниям

--- client/app/modules/Footer/Footer.jsx
+++ client/app/modules/Footer/Footer.jsx
@@ -3,11 +3,11 @@ import './Footer.scss';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import cross from '~/assets/img/crest_icon.svg';
-import facebook from '~/assets/img/facebook.svg';
-import youtube from '~/assets/img/youtube.svg';
-import vkontakte from '~/assets/img/vkontakte.svg';
-import instagram from '~/assets/img/insta.svg';
+import crossImg from '~/assets/img/crest_icon.svg';
+import facebookImg from '~/assets/img/facebook.svg';
+import youtubeImg from '~/assets/img/youtube.svg';
+import vkontakteImg from '~/assets/img/vkontakte.svg';
+import instagramImg from '~/assets/img/insta.svg';
 
 
 const Footer = (props) => {
@@ -15,34 +15,34 @@ const Footer = (props) => {
         <footer>
             <div className="container-fluid">
                 <div className="container-row layer">
-                    <div className="layer layer-1">
+                    <div className="layer layer-logo">
                         <Link to="/" data-toggle="tooltip" title="Перейти на главную страницу">
                             <span className="footer-logo">eLA</span>
                         </Link>
                     </div>
-                    <div className="layer layer-2">
+                    <div className="layer layer-links-1">
                         <div className="layer__text">
                             <Link to="/"><p>О проекте</p></Link>
                             <Link to="/"><p id="terms-policy">Условия и политика</p></Link>
                         </div>
                     </div>
-                    <div className="layer layer-3">
+                    <div className="layer layer-links-2">
                         <div className="layer__text">
                             <Link to="/"><p>Справка</p></Link>
                             <Link to="/"><p>Поддержка</p></Link>
                             <Link to="/"><p>Приложения</p></Link>
                         </div>
                     </div>
-                    <div className="layer layer-4">
+                    <div className="layer layer-social">
                         <div className="footer-icons">
-                            <img className="footer-icons__cross" src={cross} alt=""/>
-                            <a href="https://www.facebook.com/"><img className="footer-icons__facebook" src={facebook}
+                            <img className="footer-icons__cross" src={crossImg} alt=""/>
+                            <a href="https://www.facebook.com/"><img className="footer-icons__facebook" src={facebookImg}
                                                                      alt="Facebook"/></a>
-                            <a href="https://www.youtube.com/"><img className="footer-icons__youtube" src={youtube}
+                            <a href="https://www.youtube.com/"><img className="footer-icons__youtube" src={youtubeImg}
                                                                     alt="Youtube"/></a>
-                            <a href="https://vk.com/"><img className="footer-icons__vkontakte" src={vkontakte}
+                            <a href="https://vk.com/"><img className="footer-icons__vkontakte" src={vkontakteImg}
                                                            alt="Vkontakte"/></a>
-                            <a href="https://www.instagram.com/"><img className="footer-icons__instagram" src={instagram}
+                            <a href="https://www.instagram.com/"><img className="footer-icons__instagram" src={instagramImg}
                                                                       alt="Instagram"/></a>
                         </div>
                     </div>

881dca27adf4b2c24e41f9f2a38793a7bf849a09
Andrey <35691656+fufylev@users.noreply.github.com>
2019-12-06 20:27:33 +0300

    166. Подтянуть картинки слов на страницу с тестом слов
    Папку с картинками положили на data-server
    создали для нее API для доступа к контейнеру с картинками.
    
    в данном компоненте я указал путь до картинки и динамически их подтягиваем.
    Чтобы компонент не отображался вперед новой картинки использовал ```React.lazy()```

---
+++ client/app/modules/VocabularyGuessWord/components/WordPicture/WordPicture.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const WordPicture = (props) => {
+    const defaultImgSrc = 'https://placehold.it/300x200';
+    const currentImgDomain = 'http://ela-data-service.abirula.com/api/v1/Pictures/Vocabulary';
+    const {currentTask} = props;
+
+    return (
+        <img src={currentTask.imgUrl ? `${currentImgDomain}/${currentTask.imgUrl.trim()}`: defaultImgSrc}
+            className="rounded-lg shadow border border-default tasks__img"
+            alt="Картинка текущего слова"
+        />
+    );
+};
+
+export default WordPicture;

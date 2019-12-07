92ffcfdc2d0ddd3ed8327f03192149380afba5cf
victoriaCat <katysheva.victoria@gmail.com>
2019-12-07 19:53:10 +0300

    164. Добавлен базовый функционал

--- client/app/layouts/MainPage/MainPage.jsx
+++ client/app/layouts/MainPage/MainPage.jsx
@@ -3,7 +3,7 @@ import './MainPage.scss';
 import React from 'react';
 
 import TopBlockMainPage from '~/modules/TopBlockMainPage/containers/TopBlockMainPage.jsx';
-import MiddleBlockMainPage from '~/modules/MiddleBlockMainPage/MiddleBlockMainPage.jsx';
+import MiddleBlockMainPage from '~/modules/MiddleBlockMainPage/containers/MiddleBlockMainPage.jsx';
 import BottomBlockMainPage from '~/modules/BottomBlockMainPage/BottomBlockMainPage.jsx';
 
 export default function MainPage() {
